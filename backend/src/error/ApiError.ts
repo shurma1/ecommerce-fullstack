4e0de093883c4293cbdc70a7946fce24e4421143
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-02-20 11:33:02 +0300

    add api error

---
+++ backend/src/error/ApiError.ts
@@ -0,0 +1,25 @@
+export class ApiError extends Error {
+	status: number;
+	message: string;
+	constructor(status: number, message: string) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+
+	static badRequest(message: string){
+		return new ApiError(400, message);
+	}
+
+	static conflict(message: string){
+		return new ApiError(409, message);
+	}
+
+	static internal(message: string){
+		return new ApiError(500, message);
+	}
+
+	static forbidden(message: string){
+		return new ApiError(403, message);
+	}
+}
